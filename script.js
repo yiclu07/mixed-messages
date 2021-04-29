26bb16efa807d22b44216a6780956f476aed7718
Red Roc <yi.c.lu07@gmail.com>
2021-04-28 22:42:40 -0400

    Add message components

---
+++ script.js
@@ -0,0 +1,32 @@
+// Generates sentences relating to Magic: the Gathering
+const msgComponents = {
+  verb: [
+    'are probably a fan of',
+    'look like you enjoy',
+    'look like you just tried',
+    'are the type of player who hates',
+    'should be cautious for this in the near future:'
+  ],
+  nouns: [
+    'mono-white.',
+    'playing against STACKS.',
+    'casting Armageddon.',
+    'resolving a Primal Surge.',
+    'Grand Arbiter Augustin IV.',
+    'Narset Extra Turns.',
+    'pulling good cards.',
+    'proxying basic lands.',
+    'getting your spell countered.',
+    `Dragon's Maze.`,
+    `a cool Maze's End deck.`
+  ],
+  quotee: [
+    `Mark Rosewater, probably`,
+    `Zurgo Bellstriker`,
+    `Ugin, the Spirit Dragon`,
+    `Braids, Cabal Minion`,
+    `Squee, the Immortal`,
+    `The Professor`,
+    `Rudy the Magic Guy`
+  ]
+}
